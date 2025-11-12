1e2c388b627e275f184fc8ed666f6a1a050f0f43
Bùi Anh Tuấn <buituan29072003@gmail.com>
2025-11-12 17:44:45 +0700

    finish the Home, Movies, Favorite and MovieDetails pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 import './index.css'
 import App from './App'
 
@@ -11,7 +12,9 @@ if(!PUBLISHABLE_KEY) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>,
 )
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import HeroSection from '../components/HeroSection'
+import FeaturedSection from '../components/FeaturedSection'
+import TrailerSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
     <>
-      
+      <HeroSection />
+      <FeaturedSection />
+      <TrailerSection />
     </>
   )
 }
